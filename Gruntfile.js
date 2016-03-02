d7de8e43b99bf3a4924fbf069d201b081d8d8e7f
Ryan Kosai <ryan@pioneersquarelabs.com>
2016-03-01 20:57:58 -0800

    Base deploy scripts.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            build: {
+                files: {
+                    "release/index.css": "app/css/*.scss",
+                }
+            }
+        },
+        copy: {
+            build: {
+                files: [
+                    {
+                        src: 'src/index.html',
+                        dest: 'release/index.html'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'src/img/',
+                        src: '**',
+                        dest: 'release/img/'
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // Default task(s).
+    grunt.registerTask('default', ['less', 'copy']);
+};
